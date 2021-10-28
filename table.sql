aa38922c235754427aaa8177d0dbca1dcaec5220
Ethan Berman <bermane@noyce.whitman.edu>
2021-10-28 12:18:48 -0700

    first

---
+++ table.sql
@@ -0,0 +1,68 @@
+use bermane_db;
+
+drop table if exists quantity;
+drop table if exists recipe;
+drop table if exists review;
+drop table if exists ingredient
+drop table if exists user;
+
+create table user (
+    uid int autoincrement,
+    username varchar(40) not null,
+    email varchar(40) not null,
+    password varchar(40) not null,
+    restrictions enum('vegan', 'vegetarian', 'pescetarian', 'peanut allergy', 'GF', 'DF')
+    primary key (uid)
+)
+ENGINE = InnoDB;
+
+create table recipe (
+    rid int autoincrement,
+    author int,
+    name varchar(50) not null,
+    preptime int,
+    tools enum("oven", "stove", "knife", "microwave")
+    description text not null,
+    category enum("mexican", "french", "greek", "chinese", "american", "pasta")
+    foreign key (author) references user(uid)
+        on delete restrict
+    primary key(rid)
+    index(rid)
+)
+ENGINE = InnoDB;
+
+create table review (
+    revid int autoincrement,
+    author int not null,
+    recipe int not null,
+    primary key(revid),
+    index(revid),
+    foreign key(author) references user(uid)
+        on delete restrict
+    foreign key(recipe) references recipe(rid)
+        on delete restrict
+)
+ENGINE = InnoDB;
+
+create table ingredient (
+    iid int autoincrement,
+    name varchar(50) not null,
+    cost float,
+    primary key(iid)
+)
+ENGINE = InnoDB;
+
+create table quantity (
+    qid int autoincrement not null,
+    ingredient int not null,
+    recipe int not null,
+    quantity float not null,
+    unit enum("cup", "pound", "count", "tbsp", "tsp", "kilogram", "gram", "slices")
+    primary key(qid),
+    index (qid),
+    foreign key (ingredient) references ingredient(iid)
+        on delete restrict,
+    foreign key (recipe) references recipe(rid)
+        on delete restrict,
+)
+ENGINE = InnoDB;
