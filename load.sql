c8651026b1a1873c044dc677102abaa7efa4a012
Ethan Berman <bermane@noyce.whitman.edu>
2021-10-28 12:51:14 -0700

    most recent

--- load.sql
+++ load.sql
@@ -1,15 +1,17 @@
 use bermane_db;
 
-insert into ingredient(name, cost) 
+insert into ingredient(cost, name) 
 values (1.49, "Butter(unsalted)"),
     (3.49, "Wonder Bread"),
     (2.29, "American Cheese");
-insert into quantity(ingredient, recipe, quantity, unit)
-values (0, 0, 1, "tbsp"),
-        (1, 0, 2, "slices"),
-        (2, 0, 2, "slices");
+
 insert into user(username, email, password, restrictions)
 values ("eberman", "bermane@whitman.edu", "lol", "DF");
+
 insert into recipe(author, name, tools, preptime, description, category)
-values (0, "Grilled Cheese", "Stove", 15, "The classic american grilled cheese", "american");
+values (1, "Grilled Cheese", "Stove", 15, "The classic american grilled cheese", "american");
                                         
+insert into quantity(ingredient, recipe, quantity, unit)
+values (1, 1, 1, "tbsp"),
+        (2, 1, 2, "slices"),
+        (3, 1, 2, "slices");
--- table.sql
+++ table.sql
@@ -1,51 +1,54 @@
 use bermane_db;
 
 drop table if exists quantity;
-drop table if exists recipe;
+drop table if exists ingredient;
 drop table if exists review;
-drop table if exists ingredient
+drop table if exists recipe;
 drop table if exists user;
 
 create table user (
-    uid int autoincrement,
+    uid int auto_increment,
     username varchar(40) not null,
     email varchar(40) not null,
     password varchar(40) not null,
-    restrictions enum('vegan', 'vegetarian', 'pescetarian', 'peanut allergy', 'GF', 'DF')
+    restrictions enum('vegan', 'vegetarian', 'pescetarian', 'peanut allergy', 'GF', 'DF'),
     primary key (uid)
 )
 ENGINE = InnoDB;
 
 create table recipe (
-    rid int autoincrement,
-    author int,
+    rid int auto_increment,
+    author int not null,
     name varchar(50) not null,
     preptime int,
-    tools enum("oven", "stove", "knife", "microwave")
+    tools enum("oven", "stove", "knife", "microwave"),
     description text not null,
-    category enum("mexican", "french", "greek", "chinese", "american", "pasta")
+    category enum("mexican", "french", "greek", "chinese", "american", "pasta"),
+    primary key(rid),
+    index(rid),
     foreign key (author) references user(uid)
-        on delete restrict
-    primary key(rid)
-    index(rid)
+        on delete cascade
+        on update cascade
+    
 )
 ENGINE = InnoDB;
 
 create table review (
-    revid int autoincrement,
+    revid int auto_increment,
     author int not null,
     recipe int not null,
     primary key(revid),
     index(revid),
     foreign key(author) references user(uid)
-        on delete restrict
+        on delete restrict,
     foreign key(recipe) references recipe(rid)
         on delete restrict
+        on update cascade
 )
 ENGINE = InnoDB;
 
 create table ingredient (
-    iid int autoincrement,
+    iid int auto_increment,
     name varchar(50) not null,
     cost float,
     primary key(iid)
@@ -53,16 +56,18 @@ create table ingredient (
 ENGINE = InnoDB;
 
 create table quantity (
-    qid int autoincrement not null,
+    qid int auto_increment not null,
     ingredient int not null,
     recipe int not null,
     quantity float not null,
-    unit enum("cup", "pound", "count", "tbsp", "tsp", "kilogram", "gram", "slices")
+    unit enum("cup", "pound", "count", "tbsp", "tsp", "kilogram", "gram", "slices"),
     primary key(qid),
     index (qid),
     foreign key (ingredient) references ingredient(iid)
-        on delete restrict,
+        on delete restrict
+        on update cascade,
     foreign key (recipe) references recipe(rid)
-        on delete restrict,
+        on delete restrict
+        on update cascade
 )
 ENGINE = InnoDB;
